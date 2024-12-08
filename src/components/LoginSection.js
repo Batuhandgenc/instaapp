555c0280417fdb9f522d186bdcbcfcbc4ba33877
Batu han <batuhandgenc@gmail.com>
2024-12-08 01:00:04 +0000

    son1

--- src/components/LoginSection.js
+++ src/components/LoginSection.js
@@ -1,4 +1,20 @@
 import React from 'react';
+
+function LoginSection() {
+  return (
+    <div className="login-section">
+      <button className="login-button">
+        <img src="instagram-icon.png" alt="Instagram Logo" className="button-icon" />
+        Instagram ile Giriş Yap
+      </button>
+    </div>
+  );
+}
+
+export default LoginSection;
+
+/*
+import React from 'react';
 import './LoginSection.css'; 
 
 const LoginButton = () => {
@@ -12,3 +28,4 @@ const LoginButton = () => {
 };
 
 export default LoginButton;
+*/
